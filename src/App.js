cd401c019122501ba89ac4071a08a24c45f63385
VictorW-dev <victorwinicius.bs@gmail.com>
2023-09-07 21:44:58 -0300

    Blocking pages that need authentication and implementing About page

--- src/App.js
+++ src/App.js
@@ -46,14 +46,14 @@ function App() {
       <AuthProvider value={{ user }} >
         <BrowserRouter>
           <Navbar />
-          <div className='container'>
+          <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path="/login" element={<Login />} />
-              <Route path='/register' element={<Register />} />
-              <Route path="/posts/create" element={<CreatePost />} />
-              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+              <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
+              <Route path="/posts/create" element={user ? <CreatePost /> : <Navigate to="/login" />} />
+              <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
             </Routes>
           </div>
           <Footer />

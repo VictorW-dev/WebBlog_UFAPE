51c3f08929e0cd4f7cb31fa8b7ad1564efbf7810
VictorW-dev <victorwinicius.bs@gmail.com>
2023-09-09 07:54:06 -0300

    Structuring the home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,35 @@
 // CSS
 import styles from './Home.module.css';
 
+// hooks
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+
 const Home = () => {
+
+    const [query, setQuery] = useState("");
+    const [posts] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div>
-            <h1>Página inicial</h1>
+        <div className={styles.home}>
+            <h1>Veja os nossos posts mais recentes</h1>
+            <fomr onSubmit={handleSubmit} className={styles.search_form}>
+                <input type="text" placeholder="Ou busque por tags..." onChange={(e) => setQuery(e.target.value)} />
+                <button className="btn btn-dark">Pesquisar</button>
+            </fomr>
+            <div>
+                <h1>Posts...</h1>
+                {posts && posts.length === 0 && (
+                    <div>
+                        <p>Não foram encontrados posts</p>
+                        <Link to="/posts/create" className="btn">Criar primeiro post</Link>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }

71ad9caed3f370f7757b94fe5e386897ce0e7ca8
VictorW-dev <victorwinicius.bs@gmail.com>
2023-09-09 15:33:25 -0300

    Single post page CSS

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -10,11 +10,15 @@ const Post = () => {
     const { document: post, loading } = useFetchDocument("posts", id);
 
     return (
-        <div>
+        <div className={styles.post_container}>
             {loading && <p>Carregando post...</p>}
             {post && (
                 <>
                     <h1>{post.title}</h1>
+                    <img className={styles.post_img} src={post.image} alt={post.title} />
+                    <p>{post.body}</p>
+                    <h3>Este post trata sobre:</h3>
+                    <div className={styles.tags}>{post.tagsArray.map((tag) => (<p key={tag}><span>#</span>{tag}</p>))}</div>
                 </>
             )}
         </div>
